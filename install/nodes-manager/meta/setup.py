14bdc7ddac49b246a4ae0e33c638ba7ba0a703cf
fabregas <kksstt@gmail.com>
2011-08-09 18:01:48 +0300

    nodes-manager packet created

---
+++ install/nodes-manager/meta/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import sys
+from version import VERSION
+
+setup(
+    name = "NodesManager",
+    version = VERSION,
+    maintainer = "Konstantin Andrusenko",
+    maintainer_email = "blikproject@gmail.com",
+    description = "Nodes Manager module of Blik Cloud management system",
+    packages = ['blik', 'blik.nodesManager', 'blik.nodesManager.plugins'],  # include all packages under src
+    scripts = ['bin/nodesManager'],
+    license = 'GPLv3',
+    zip_safe = True
+)
+
