18dd6de703a1d97d00af38aea753464e083083c1
fabregas <kksstt@gmail.com>
2011-12-31 16:39:48 +0200

    implemented operations plugins static validation in cm-package

--- tests/create_vbox_machines.py
+++ tests/create_vbox_machines.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 import re
 import uuid
@@ -10,14 +11,19 @@ def generate_cluster(cluster_name, machines_count, memory, iface):
     cluster_spec_dir = os.path.join(CLUSTER_DIR, cluster_name)
     commands = ['mkdir -p %s'%cluster_spec_dir]
     del_commands = []
+    start_commands = []
+    stop_commands = []
 
     for num in range(machines_count):
-        commands.append('VBoxManage createvm --name %s%02i --register --basefolder %s --uuid %s%02i'%(cluster_name, num, cluster_spec_dir, uuid_prefix, num))
+        hostname = '%s%02i'%(cluster_name, num)
+        commands.append('VBoxManage createvm --name %s --register --basefolder %s --uuid %s%02i'%(hostname, cluster_spec_dir, uuid_prefix, num))
         commands.append('VBoxManage modifyvm %s%02i --memory %s --pae on --boot1 net --nic1 bridged --cableconnected1 on --bridgeadapter1 %s --audio none'%(cluster_name, num,memory, iface))
+        start_commands.append('VBoxManage startvm %s --type headless'%hostname)
+        stop_commands.append('VBoxManage controlvm %s poweroff'%hostname)
         del_commands.append('VBoxManage unregistervm %s%02i'%(uuid_prefix, num))
 
     del_commands.append('rm -rf %s'%cluster_spec_dir)
-    return commands, del_commands
+    return commands, del_commands, start_commands, stop_commands
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -42,7 +48,7 @@ if __name__ == '__main__':
         if len(sys.argv) > 5:
             iface = sys.argv[5]
 
-        commands, del_commands = generate_cluster(cluster_name, machines_count, memory, iface)
+        commands, del_commands, start_commands, stop_commands = generate_cluster(cluster_name, machines_count, memory, iface)
 
         f = open('%s_register.sh'%cluster_name, 'w')
         for command in commands:
@@ -53,4 +59,19 @@ if __name__ == '__main__':
         for command in del_commands:
             f.write('%s\n'%command)
         f.close()
+
+        f = open('%s_start.sh'%cluster_name, 'w')
+        for command in start_commands:
+            f.write('%s\n'%command)
+        f.close()
+
+        f = open('%s_stop.sh'%cluster_name, 'w')
+        for command in stop_commands:
+            f.write('%s\n'%command)
+        f.close()
+
+        print('Scripts for cluster management saved to current directory into %s_*.sh files.'%cluster_name)
+    else:
+        sys.stderr.write('Command "%s" is not supported\n'%command)
+        sys.exit(1)
 
